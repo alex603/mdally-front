5be483598fec01874603ffd4ff4f32c9c5ab8828
Alexander Lee <alex@modv-ve702-4761.apng.wlan.private.upenn.edu>
2018-08-09 10:58:35 -0400

    message

---
+++ bs4admin-light/assets/js/window.js
@@ -0,0 +1,12 @@
+
+var winGoogle = window.open('https://google.com', '_blank');
+var winBing = window.open('https://bing.com', '_blank');
+var winYahoo = window.open('https://yahoo.com', '_blank');
+
+function capture() {
+window.open('https://yahoo.com', '_blank');
+};
+//close the windows
+// winGoogle.close();
+// winBing.close();
+// winYahoo.close();
